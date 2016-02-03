1097aed25a560878356f02fae240979d7e658fe2
GeorgEdy <eduard.dinu94@yahoo.ro>
2016-02-03 16:39:52 +0200

    receive and spend routing

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
 $(document).ready(function() {
-    $('.input-group.date').datetimepicker({
-        format: 'YYYY-MM-DD HH:mm'
+ $('.input-group.date').datetimepicker({
+ format: 'YYYY-MM-DD HH:mm'
+ });
+ });
+angular.module('myApp', ['ngRoute'])
+
+    .config(function ($routeProvider) {
+        $routeProvider.when("/",
+            {
+                templateUrl: "history.html"
+            })
+            .when('/spend.html',
+                {
+                    templateUrl: 'spend.html'
+                })
+            .when('/receive.html',
+                {
+                    templateUrl: 'receive.html'
+                })
     });
-});
